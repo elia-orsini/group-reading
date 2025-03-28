3c8bdef119a066c3f71f4d4fb73be3f7da13e8d6
Your name here <elia.orsini@hotmail.com>
2025-03-28 14:10:21 +0000

    chore: fetch request

--- src/app/recap/page.tsx
+++ src/app/recap/page.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 
 async function getChapterReadings() {
-  const res = await fetch("http://localhost:3000/api/readings", { next: { revalidate: 60 } });
+  const res = await fetch("/api/readings", { next: { revalidate: 60 } });
 
   if (!res.ok) throw new Error("Failed to fetch readings");
   return res.json();
